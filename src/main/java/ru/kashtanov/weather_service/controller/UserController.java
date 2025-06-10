cf1b40c6fd4aad6a3f443157b65b82fade90977e
Viktor Kashtanov <pistoletviktor@gmail.com>
2025-06-10 11:57:42 +0300

    init

---
+++ src/main/java/ru/kashtanov/weather_service/controller/UserController.java
@@ -0,0 +1,17 @@
+package ru.kashtanov.weather_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kashtanov.weather_service.models.User;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/single_user")
+    public User getUser() {
+        System.out.println("get user");
+        return new User("sigma", "boy");
+    }
+}
